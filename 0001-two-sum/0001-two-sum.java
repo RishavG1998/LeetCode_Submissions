2478752bd1a013d1f181c78b1cbf6788d762dcea
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-11-23 00:47:09 +0530

    Time: 9 ms (55.05%), Space: 42.9 MB (99.61%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int numsSize = nums.length;
         
-        // Brute Force
+// Brute Force
         // for (int i=0; i<numsSize-1; i++) {
         //     for (int j=i+1; j<numsSize; j++) {
         //         if (nums[i] + nums[j] == target) {
@@ -12,20 +12,51 @@ class Solution {
         // }
         // return new int[]{};
 
-        // Two-pass Hash Table
-        
-        // Create hash table
-        Map<Integer, Integer> numsMap = new HashMap<>();
+// Two-pass Hash Table
+        // First pass: Populate the HashMap
+        // Map<Integer, Integer> numsMap = new HashMap<>();
+        // for (int i=0; i<numsSize; i++) {
+        //     numsMap.put(nums[i], i);
+        // }
+
+        // Second pass: Check for the complement
+        // for (int i=0; i<numsSize; i++) {
+        //     int complement = target - nums[i];
+        //     if (numsMap.containsKey(complement) && numsMap.get(complement) != i) {
+        //         return new int[]{i, numsMap.get(complement)};
+        //     }
+        // }
+        // return new int[]{};
+
+// One-pass Hash Table
+        // Map<Integer, Integer> numsMap = new HashMap(){};
+        // for (int i=0; i<numsSize; i++) {
+        //     int complement = target - nums[i];
+        //     if (numsMap.containsKey(complement)) {
+        //         return new int[]{numsMap.get(complement), i};
+        //     }
+        //     numsMap.put(nums[i], i);
+        // }
+        // return new int[]{}; // No solution found
+
+// Two-pointer Approach
+        int numsWithIndex[][] = new int[nums.length][2];
+        // Creating 2-d array similar to hashmap
         for (int i=0; i<numsSize; i++) {
-            // storing position corresponding to the element
-            numsMap.put(nums[i], i);
+            numsWithIndex[i][0] = nums[i];
+            numsWithIndex[i][1] = i;
         }
-        
-        // Find the complement
-        for (int i=0; i<numsSize; i++) {
-            int complement = target - nums[i];
-            if (numsMap.containsKey(complement) && numsMap.get(complement) != i) {
-                return new int[]{numsMap.get(complement), i};
+        Arrays.sort(numsWithIndex, Comparator.comparingInt(arr -> arr[0]));
+        int left = 0;
+        int right = numsSize - 1;
+        while(left < right) {
+            int total = numsWithIndex[left][0] + numsWithIndex[right][0];
+            if (total < target) {
+                left += 1;
+            } else if (total > target) {
+                right -= 1;
+            } else {
+                return new int[]{numsWithIndex[left][1], numsWithIndex[right][1]};
             }
         }
         return new int[]{};
