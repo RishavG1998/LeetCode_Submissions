d4bc93ceaf6e5bd8d108f7a7c0b01879fa8cbae8
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-11-29 10:18:29 +0530

    Time: 3 ms (66.32%), Space: 13.6 MB (99.79%) - LeetHub

--- 0066-plus-one/0066-plus-one.swift
+++ 0066-plus-one/0066-plus-one.swift
@@ -1,21 +1,33 @@
 class Solution {
     func plusOne(_ digits: [Int]) -> [Int] {
+        // var tempDigits = digits
+        // tempDigits[tempDigits.count - 1] += 1
+        // for index in stride(from: tempDigits.count-1, to: 0, by: -1) {
+        //     if tempDigits[index] <= 9 {
+        //         return tempDigits
+        //     } else {
+        //         tempDigits[index] = 0
+        //         tempDigits[index-1] += 1
+        //     }
+        // }
+        // if tempDigits[0] <= 9 {
+        //     return tempDigits
+        // } else {
+        //     tempDigits[0] = 0
+        //     tempDigits.insert(1, at: 0)
+        // }
+        // return tempDigits
+
         var tempDigits = digits
-        tempDigits[tempDigits.count - 1] += 1
-        for index in stride(from: tempDigits.count-1, to: 0, by: -1) {
-            if tempDigits[index] <= 9 {
+        for index in stride(from: tempDigits.count-1, to: -1, by: -1) {
+            if tempDigits[index] < 9 {
+                tempDigits[index] += 1
                 return tempDigits
             } else {
                 tempDigits[index] = 0
-                tempDigits[index-1] += 1
             }
         }
-        if tempDigits[0] <= 9 {
-            return tempDigits
-        } else {
-            tempDigits[0] = 0
-            tempDigits.insert(1, at: 0)
-        }
+        tempDigits.insert(1, at: 0)
         return tempDigits
     }
 }
