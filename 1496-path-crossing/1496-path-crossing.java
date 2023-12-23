dfdbcaea235f2c5bfe932ca68a8daf545b916d0d
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-12-24 01:17:56 +0530

    Time: 5 ms (42.92%), Space: 42.6 MB (5.75%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.java
+++ 1496-path-crossing/1496-path-crossing.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean isPathCrossing(String path) {
+        // Set<String> visited = new HashSet<>();
         List<String> coordList = new ArrayList<String>();
 	    int x = 0, y = 0;
-	    String presentCoord = x + "," + y;
-	    coordList.add(presentCoord);
+	    // String presentCoord = x + "," + y;
+	    // coordList.add(presentCoord);
+        coordList.add("0,0");
 	    // System.out.println(presentCoord);
 	    for(int i=0; i<path.length(); i++) {
 	        if (path.charAt(i) == 'N') {
@@ -15,14 +17,13 @@ class Solution {
 	        } else if (path.charAt(i) == 'S') {
 	            y -= 1;
 	        }
-	        presentCoord = x + "," + y;
+	        String presentCoord = x + "," + y;
 	        // System.out.println(presentCoord);
 	        if (coordList.contains(presentCoord)) {
 	            return true;
-	        } else {
-	           // add to the list
-	           coordList.add(presentCoord);
 	        }
+            // add to the list
+	        coordList.add(presentCoord);
 	    }
 	    return false;
     }
