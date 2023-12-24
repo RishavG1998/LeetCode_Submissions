936c45978709158d9bdce7e314a3770f0accdbd3
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-12-24 23:26:40 +0530

    Time: 3 ms (92.13%), Space: 41.9 MB (12.79%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -24,23 +24,35 @@ class Solution {
 
         
     // 2M
+        // int countStartingWith0 = 0;
+        // int countStartingWith1 = 0;
+        // for (int i=0; i<s.length(); i++) {
+        //     if (i%2 == 0) {
+        //         if (s.charAt(i) == '0') {
+        //             countStartingWith0++;
+        //         } else {
+        //             countStartingWith1++;
+        //         }
+        //     } else {
+        //         if (s.charAt(i) == '1') {
+        //             countStartingWith0++;
+        //         } else {
+        //             countStartingWith1++;
+        //         }
+        //     }
+        // }
+        // return Math.min(countStartingWith0, countStartingWith1);
+
+    // 3M
+        int n = s.length();
         int countStartingWith0 = 0;
-        int countStartingWith1 = 0;
-        for (int i=0; i<s.length(); i++) {
-            if (i%2 == 0) {
-                if (s.charAt(i) == '0') {
-                    countStartingWith0++;
-                } else {
-                    countStartingWith1++;
-                }
-            } else {
-                if (s.charAt(i) == '1') {
-                    countStartingWith0++;
-                } else {
-                    countStartingWith1++;
-                }
+
+        for(int i=0; i<n; i++) {
+            if (s.charAt(i) - '0' == i % 2) {
+                countStartingWith0++;
             }
         }
-        return Math.min(countStartingWith0, countStartingWith1);
+
+        return Math.min(countStartingWith0, n - countStartingWith0);
     }
 }
