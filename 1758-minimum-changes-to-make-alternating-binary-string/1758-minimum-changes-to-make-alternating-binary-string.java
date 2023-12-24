7aac4283bb513cb8e1d328dcbc71e8013f661c79
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-12-24 23:18:19 +0530

    Time: 4 ms (74.75%), Space: 42.3 MB (7.87%) - LeetHub

---
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int minOperations(String s) {
+
+    // 1M
+        // int count1 = 0, count2 = 0;
+        // int binary = 0;
+        // for (int i=0; i<s.length(); i++) {
+        //     if (i%2 == 0) {
+        //         binary++;
+        //     } else {
+        //         binary--;
+        //     }
+        //     String binaryStr = Integer.toString(binary);
+        //     String character = "" + s.charAt(i);
+        //     // Stirng char1 = String.valueOf(s.charAt(i))
+        //     if (!binaryStr.equals(character)) {
+        //         count1++;
+        //     }
+        //     if (binaryStr.equals(character)) {
+        //         count2++;
+        //     }
+        // }
+        // return Math.min(count1, count2);
+
+        
+    // 2M
+        int countStartingWith0 = 0;
+        int countStartingWith1 = 0;
+        for (int i=0; i<s.length(); i++) {
+            if (i%2 == 0) {
+                if (s.charAt(i) == '0') {
+                    countStartingWith0++;
+                } else {
+                    countStartingWith1++;
+                }
+            } else {
+                if (s.charAt(i) == '1') {
+                    countStartingWith0++;
+                } else {
+                    countStartingWith1++;
+                }
+            }
+        }
+        return Math.min(countStartingWith0, countStartingWith1);
+    }
+}
