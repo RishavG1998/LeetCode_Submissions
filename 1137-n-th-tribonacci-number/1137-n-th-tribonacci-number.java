d8622a56ca5a66504825a059aa63766cfcea1c8b
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2023-12-25 00:01:58 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (5.28%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int tribonacci(int n) {
+
+    // 1M: simple recursion
+        // if (n <= 1) {
+	    //     return n;
+	    // }
+	    // if (n == 2) {
+	    //     return 1;
+	    // }
+	    
+	    // return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3); 
+
+    // 2M: iteration
+        // if (n <= 1) {
+        //     return n;
+        // }
+        // if (n == 2) {
+        //     return 1;
+        // }
+        // int tLast = 0, sLast = 1, last = 1;
+	    // for(int i=0; i<n-2; i++) {
+	    //     int temp = tLast;
+	    //    // int temp2 = sLast;
+	    //     tLast = sLast;
+	    //     sLast = last;
+	    //     last = temp + tLast + sLast;
+	    // }
+	    // return last;
+
+    // 3M: DP
+        int[] dp = new int[n+1];
+	    Arrays.fill(dp, -1);
+	    return solve(n, dp);
+    }
+
+    public int solve(int n, int[] dp) {
+        if (n <= 1) {
+	        return n;
+	    }
+	    if (n == 2) {
+	        return 1;
+	    }
+	    if (dp[n] != -1) {
+	        return dp[n];
+	    }
+	    
+	    return dp[n] = solve(n-1, dp) + solve(n-2, dp) + solve(n-3, dp);
+    }
+}
