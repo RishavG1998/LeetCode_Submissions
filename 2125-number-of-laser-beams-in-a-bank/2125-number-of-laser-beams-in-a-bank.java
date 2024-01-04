96d0a8e9a785991f021904e27fe1164b0d7d173c
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2024-01-04 09:34:56 +0530

    Time: 8 ms (98.84%), Space: 45.1 MB (23.02%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.java
@@ -1,16 +1,48 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int ans = 0, prev = 0, cnt = 0;
-        for (String s: bank) {
-            cnt = 0;
-            for (int i=0; i<s.length(); i++) {
-                if (s.charAt(i) == '1') cnt++;
+        // int ans = 0, prev = 0, cnt = 0;
+        // for (String s: bank) {
+        //     cnt = 0;
+        //     for (int i=0; i<s.length(); i++) {
+        //         if (s.charAt(i) == '1') cnt++;
+        //     }
+        //     if (cnt > 0) {
+        //         ans += cnt * prev;
+        //         prev = cnt;
+        //     }
+        // }
+        // return ans;
+
+        // 2M:
+        // int ans = 0, temp = 0;
+        // for (String str : bank) {
+        //     int n = str.replaceAll("[^1]", "").length();
+        //     if (n == 0) continue;
+        //     ans += temp * n;
+        //     temp = n;
+        // }
+        // return ans;
+
+        // 3M:
+        int prevRowCnt = 0, total = 0;
+        for (String row: bank) {
+            int curRowCnt = calc(row);
+
+            if (curRowCnt == 0) {
+                continue;
             }
-            if (cnt > 0) {
-                ans += cnt * prev;
-                prev = cnt;
-            }
+            total += prevRowCnt * curRowCnt;
+            prevRowCnt = curRowCnt;
         }
-        return ans;
+        return total;
+    }
+
+    private int calc(String s) {
+        int cnt = 0;
+        for (char c: s.toCharArray()) {
+            // if (c - '1' == 0) cnt += 1;
+            cnt += c - '0';
+        }
+        return cnt;
     }
 }
