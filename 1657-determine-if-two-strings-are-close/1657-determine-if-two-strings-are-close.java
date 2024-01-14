09758439d47f2985b957e3d8f23832226dc74838
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2024-01-15 00:28:14 +0530

    Time: 10 ms (92.33%), Space: 45.8 MB (10.43%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,28 +1,12 @@
 class Solution {
+    final static int n = 26;
     public boolean closeStrings(String word1, String word2) {
         if (word1.length() != word2.length()) {
             return false;
         }
 
-	// Map<Character, Integer> word1Map = new HashMap<>();
-	// Map<Character, Integer> word2Map = new HashMap<>();
-	// for (char c : word1.toCharArray()) {
-	// 	word1Map.put(c, word1Map.getOrDefault(c, 0) + 1);
-	// }
-	// for (char c : word2.toCharArray()) {
-	// 	word2Map.put(c, word2Map.getOrDefault(c, 0) + 1);
-	// }
-	// if (!word1Map.keySet().equals(word2Map.keySet())) {
-	// 	return false;
-	// }
-	// List<Integer> word1FrequencyList = new ArrayList<>(word1Map.values());
-	// List<Integer> word2FrequencyList = new ArrayList<>(word2Map.values());
-	// Collections.sort(word1FrequencyList);
-	// Collections.sort(word2FrequencyList);
-	// return word1FrequencyList.equals(word2FrequencyList);
-
-        int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
+        int[] freq1 = new int[n];
+        int[] freq2 = new int[n];
 
         for (char ch: word1.toCharArray()) {
             freq1[ch - 'a']++;
@@ -32,11 +16,18 @@ class Solution {
             freq2[ch - 'a']++;
         }
 
-        for (int i=0; i<26; i++) {
-            if ((freq1[i] == 0 && freq2[i] != 0) || 
-            (freq1[i] != 0 && freq2[i] == 0)) {
+        // for (int i=0; i<n; i++) {
+        //     if ((freq1[i] == 0 && freq2[i] != 0) || 
+        //     (freq1[i] != 0 && freq2[i] == 0)) {
+        //         return false;
+        //     }
+        // }
+
+        for (int i=0; i<n; i++) {
+            if (freq1[i] == freq2[i]) 
+                continue;
+            if (freq1[i] == 0 || freq2[i] == 0)
                 return false;
-            }
         }
 
         Arrays.sort(freq1);
@@ -55,5 +46,23 @@ class Solution {
         }
 
         return true;
+
+
+        // Map<Character, Integer> word1Map = new HashMap<>();
+        // Map<Character, Integer> word2Map = new HashMap<>();
+        // for (char c : word1.toCharArray()) {
+        //     word1Map.put(c, word1Map.getOrDefault(c, 0) + 1);
+        // }
+        // for (char c : word2.toCharArray()) {
+        //     word2Map.put(c, word2Map.getOrDefault(c, 0) + 1);
+        // }
+        // if (!word1Map.keySet().equals(word2Map.keySet())) {
+        //     return false;
+        // }
+        // List<Integer> word1FrequencyList = new ArrayList<>(word1Map.values());
+        // List<Integer> word2FrequencyList = new ArrayList<>(word2Map.values());
+        // Collections.sort(word1FrequencyList);
+        // Collections.sort(word2FrequencyList);
+        // return word1FrequencyList.equals(word2FrequencyList);
     }
 }
