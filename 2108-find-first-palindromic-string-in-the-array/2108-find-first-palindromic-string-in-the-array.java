33707b965e00802cf0e6992ebe1db13e873ca57b
RishavGupta1C <91869768+RishavGupta1C@users.noreply.github.com>
2024-02-16 03:14:27 +0530

    Time: 4 ms (29.41%), Space: 45 MB (36.50%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -1,26 +1,36 @@
 class Solution {
-
-    // 2M: 100%
-    public boolean checkWordForPalindrome(String word) {
-        int i = 0, j = word.length()-1;
-        while(i<=j) {
-            if (word.charAt(i) != word.charAt(j)) {
-                return false;
-            }
-            i+=1;
-            j-=1;
-        }
-        return true;
-    }
-
     public String firstPalindrome(String[] words) {
-        // 2M
-        for (String word: words) {
-            if (checkWordForPalindrome(word)) {
-                return word;
-            }
+        for(String word: words) {
+            StringBuilder s = new StringBuilder(word);
+            String reversedWord = s.reverse().toString();
+            // System.out.println(s.reverse().toString() + " " + word);
+            if (word.equals(reversedWord)) return word;
         }
         return "";
+    }
+
+
+    // 2M: 100%
+    // public boolean checkWordForPalindrome(String word) {
+    //     int i = 0, j = word.length()-1;
+    //     while(i<=j) {
+    //         if (word.charAt(i) != word.charAt(j)) {
+    //             return false;
+    //         }
+    //         i+=1;
+    //         j-=1;
+    //     }
+    //     return true;
+    // }
+
+    // public String firstPalindrome(String[] words) {
+    //     // 2M
+    //     for (String word: words) {
+    //         if (checkWordForPalindrome(word)) {
+    //             return word;
+    //         }
+    //     }
+    //     return "";
 
 
 
@@ -42,5 +52,5 @@ class Solution {
         // return "";
 
 
-    }
+    // }
 }
