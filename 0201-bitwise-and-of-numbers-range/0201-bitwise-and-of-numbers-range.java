350c953c2c1b41f06beb4547583520c08e1dd7c0
RishavGupta1998 <91869768+RishavG1998@users.noreply.github.com>
2024-06-27 23:06:36 +0530

    Time: 29 ms (6.08%), Space: 43.8 MB (46.68%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,25 +1,25 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
         // 3M: Bit Operation solution
-        // if (left == 0) return 0;
-        // int moveFactor = 1;
-        // while (left != right) {
-        //     left >>= 1;
-        //     right >>= 1;
-        //     moveFactor <<= 1;
-        // }
-        // System.out.println(left);
-        // System.out.println(moveFactor);
-        // return left * moveFactor;
+        if (left == 0) return 0;
+        int moveFactor = 1;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            moveFactor <<= 1;
+        }
+        System.out.println(left);
+        System.out.println(moveFactor);
+        return left * moveFactor;
 
 
         // 2M:
         // Since bitwise and will always produce result smaller than
         // or equal to the number
-        while(right > left) {
-            right = right & (right-1);
-        }
-        return right;
+        // while(right > left) {
+        //     right = right & (right-1);
+        // }
+        // return right;
 
         // TLE
         // int ans = left;
